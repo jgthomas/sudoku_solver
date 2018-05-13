260d26d8210298c199bde8981f986a9008de7223
James Thomas <codeactor@protonmail.com>
2018-05-13 10:38:43 +0100

    function declarations now in header

--- src/backtracking_solver.c
+++ src/backtracking_solver.c
@@ -3,23 +3,10 @@
 #include <stdbool.h>
 #include <math.h>
 #include "grid.h"
+#include "backtracking_solver.h"
 #include "display_grid.h"
 //#include "4x4puzzle.h"
 #include "9x9puzzle.h"
-
-
-bool row_contains(Grid *puzzle, int row, int find);
-bool col_contains(Grid *puzzle, int col, int find);
-bool box_contains(Grid *puzzle, int min_row, int min_col, int find);
-bool num_allowed(Grid *puzzle, int row, int col, int num);
-int min_col(Grid *puzzle, int col);
-int min_row(Grid *puzzle, int row);
-Square *make_square(int num, bool part_of_puzzle);
-Grid *make_grid(int grid_size, int grid[grid_size][grid_size]);
-void print_puzzle(Grid *puzzle);
-bool fill_square(Grid *puzzle, int row, int col);
-void solve_puzzle(Grid *puzzle);
-void delete_puzzle(Grid *puzzle);
 
 
 int main()
