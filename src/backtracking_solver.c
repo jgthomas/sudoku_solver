e1c6c21deebfbae61d1b27fae2be9b780444db7d
James Thomas <codeactor@protonmail.com>
2018-05-21 07:37:24 +0100

    switch to first 9x9 grid

--- src/backtracking_solver.c
+++ src/backtracking_solver.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
     }
     else
     {
-            puzzle = make_grid(grid_size, grid9x9_Vhard);
+            puzzle = make_grid(grid_size, grid9x9_1);
     }
 
     printf("\nPUZZLE\n");
